ee4f0a0a3f9c675e301abe3f0d63d6f92a4d21b8
Игорь Ткаченко <zenbee111@gmail.com>
2023-01-30 21:37:34 +0300

    Added missing dictConfig

--- sqlite_to_postgres/config.py
+++ sqlite_to_postgres/config.py
@@ -18,26 +18,28 @@ SQLITE_DB_PATH = os.getenv("SQLITE_DB_PATH")
 
 CHUNK_SIZE = int(os.getenv("CHUNK_SIZE", 1000))
 
-
-
-
 log_config = {
     "version":1,
-    "root":{
+    "root":
+    {
         "handlers" : ["console"],
         "level": "DEBUG"
     },
-    "handlers":{
+    "handlers":
+    {
         "console":{
             "formatter": "std_out",
             "class": "logging.StreamHandler",
             "level": "DEBUG"
         }
     },
-    "formatters":{
+    "formatters":
+    {
         "std_out": {
             "format": "%(asctime)s : %(levelname)s : %(module)s : %(funcName)s : %(lineno)d : (Process Details : (%(process)d, %(processName)s), Thread Details : (%(thread)d, %(threadName)s))\nLog : %(message)s",
             "datefmt":"%d-%m-%Y %I:%M:%S"
         }
     },
 }
+
+config.dictConfig(log_config)
